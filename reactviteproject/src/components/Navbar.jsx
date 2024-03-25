7f7052e6fbd07dce9aab07b88dc42cc65e521a6d
santosupadhyay <itsmr.santoshupadhyay18@gmail.com>
2024-03-25 13:46:30 +0545

    Hello updated ones

--- reactviteproject/src/components/Navbar.jsx
+++ reactviteproject/src/components/Navbar.jsx
@@ -1,24 +1,33 @@
 import React from 'react'
 
 function Navbar() {
-  return (
-    <div>
-        <div className="container h-4">
-                <ul className='flex justify-center align-middle'>
-                        <li>Red</li>
-                        <li>Green</li>
-                        <li>Yellow</li>
-                        <li>Blue</li>
-                        <li>White</li>
-                        <li>OceanBlue</li>
-                        <li>Purple</li>
-                        <li>Orange</li>
-                        <li>Pink</li>
-                        <li>Black</li>
-                </ul>
-        </div>      
-    </div>
-  )
+        let backgroundColor = 'white';
+        function getRed () {
+                
+        }
+        return (
+                <>
+                        <div>
+                                <div className="container h-4">
+                                        <ul className='flex justify-center align-middle gap-5 cursor-pointer bg-slate-400 hover:color-white'>
+                                                <li><button onClick={getRed}>Red</button></li>
+                                                <li>Green</li>
+                                                <li>Yellow</li>
+                                                <li>Blue</li>
+                                                <li>White</li>
+                                                <li>OceanBlue</li>
+                                                <li>Purple</li>
+                                                <li>Orange</li>
+                                                <li>Pink</li>
+                                                <li>Black</li>
+                                        </ul>
+                                </div>
+                                <div className="mainpart w-full border-solid border-2 border-red-600 mt-2 h-96">
+
+                                </div>
+                        </div>
+                </>
+        )
 }
 
 export default Navbar
