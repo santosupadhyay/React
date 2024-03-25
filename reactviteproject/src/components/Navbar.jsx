4945917997bdfa7c7d9296bdfbee87721088cb61
santosupadhyay <itsmr.santoshupadhyay18@gmail.com>
2024-03-25 13:19:53 +0545

    navbar pushed

---
+++ reactviteproject/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div className="container h-4">
+                <ul className='flex justify-center align-middle'>
+                        <li>Red</li>
+                        <li>Green</li>
+                        <li>Yellow</li>
+                        <li>Blue</li>
+                        <li>White</li>
+                        <li>OceanBlue</li>
+                        <li>Purple</li>
+                        <li>Orange</li>
+                        <li>Pink</li>
+                        <li>Black</li>
+                </ul>
+        </div>      
+    </div>
+  )
+}
+
+export default Navbar
