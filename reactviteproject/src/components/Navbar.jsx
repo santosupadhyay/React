5cc24e1f95e85e4dc5ad592b09dd1004b66e994f
santosupadhyay <itsmr.santoshupadhyay18@gmail.com>
2024-03-28 19:35:25 +0545

    Navbar updated

--- reactviteproject/src/components/Navbar.jsx
+++ reactviteproject/src/components/Navbar.jsx
@@ -1,31 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Navbar() {
-        let backgroundColor = 'white';
-        function getRed () {
+        const [color, setColor] = useState('white');
                 
-        }
+        
         return (
-                <>
-                        <div>
-                                <div className="container h-4">
-                                        <ul className='flex justify-center align-middle gap-5 cursor-pointer bg-slate-400 hover:color-white'>
-                                                <li><button onClick={getRed}>Red</button></li>
-                                                <li>Green</li>
-                                                <li>Yellow</li>
-                                                <li>Blue</li>
-                                                <li>White</li>
-                                                <li>OceanBlue</li>
-                                                <li>Purple</li>
-                                                <li>Orange</li>
-                                                <li>Pink</li>
-                                                <li>Black</li>
-                                        </ul>
-                                </div>
-                                <div className="mainpart w-full border-solid border-2 border-red-600 mt-2 h-96">
+        <>
+        <div>
+                <div className="container h-4">
+                        <ul className='flex justify-center align-middle gap-5 cursor-pointer bg-slate-400 hover:color-white'>
+                                <li><button  onClick={ () => {setColor('red')}}>Red</button></li>
+                                <li><button  onClick={ () => {setColor('green')}}>Green</button></li>
+                                <li><button  onClick={ () => {setColor('yellow')}}>Yellow</button></li>
+                                <li><button  onClick={ () => {setColor('pink')}}>Pink</button></li>
+                                <li><button  onClick={ () => {setColor('brown')}}>Brown</button></li>
+                                <li><button  onClick={ () => {setColor('lightseagreen')}}>Lightseagreen</button></li>
+                                <li><button  onClick={ () => {setColor('black')}}>Black</button></li>
+                                <li><button  onClick={ () => {setColor('gold')}}>Gold</button></li>
+                                                
+                        </ul>
+                </div>
+                <div className="mainpart w-full border-solid border-2 border-red-600 mt-2 h-screen" style={{backgroundColor : color}}>
 
-                                </div>
-                        </div>
+                </div>
+         </div>
                 </>
         )
 }
