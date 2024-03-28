15e6f31c889d62eb4309fc83a05cd574f5d24fa0
santosupadhyay <itsmr.santoshupadhyay18@gmail.com>
2024-03-28 19:36:49 +0545

    App Js updated

--- reactviteproject/src/App.jsx
+++ reactviteproject/src/App.jsx
@@ -3,31 +3,31 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
-  // let [counter, setCounter] = useState(0);
+  let [counter, setCounter] = useState(0);
 
-  // const addValue = () => {
-  //   counter = counter +1;
-  //   setCounter(counter);
-  //   if(counter > 20){
-  //     setCounter(counter = 20);
-  //   }
-  // }
+  const addValue = () => {
+    counter = counter +1;
+    setCounter(counter);
+    if(counter > 20){
+      setCounter(counter = 20);
+    }
+  }
   
-  // const removeValue = () => {
-  //   counter = counter - 1;
-  //   setCounter(counter);
-  //   if (counter <= 0){
-  //     setCounter(counter = 0);
-  //   }
-  // }
+  const removeValue = () => {
+    counter = counter - 1;
+    setCounter(counter);
+    if (counter <= 0){
+      setCounter(counter = 0);
+    }
+  }
   return (
     <>
-    {/* <h1>Counter!</h1>
+    <h1>Counter!</h1>
 
     <h3>Counter value : {counter} </h3>
 
     <button onClick={addValue}>Add Value</button>
-    <button onClick={removeValue}>Remove value</button> */}
+    <button onClick={removeValue}>Remove value</button>
     </>
   )
 }
