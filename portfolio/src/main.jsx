0b762def5edadd278d601323747a07b164383c1f
santosupadhyay <itsmr.santoshupadhyay18@gmail.com>
2024-04-10 13:44:29 +0545

    updates

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -8,52 +8,36 @@ import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import Projects from './components/Projects/Projects.jsx'
 import Socialmedia from './components/Socialmedia/Socialmedia.jsx'
-
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     Element: <Layout />,
-//     children:[
-//       {
-//         path:'/home',
-//         Element: <Home />
-//       },
-//       {
-//         path: '/about',
-//         Element: <About />
-//       },
-//       {
-//         path: '/contact',
-//         Element: <Contact />
-//       },
-//       {
-//         path: '/projects',
-//         Element: <Projects />
-//       },
-//       {
-//         path: '/socialmedia',
-//         Element: <Socialmedia />
-//       }
-//     ]
-//   }
-
-// ])
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<Contact />} />
-      {/* <Route 
-      loader={githubInfoLoader}
-      path='github' 
-      element={<Github />}
-       /> */}
-    </Route>
-  )
-)
-
+ 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Element: <Layout />,
+    children:[
+      {
+        path:'/home',
+        Element: <Home />
+      },
+      {
+        path: '/about',
+        Element: <About />
+      },
+      {
+        path: '/contact',
+        Element: <Contact />
+      },
+      {
+        path: '/projects',
+        Element: <Projects />
+      },
+      {
+        path: '/socialmedia',
+        Element: <Socialmedia />
+      }
+    ]
+  }
+ 
+]) 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
