3bbcb64373a749602ca546da7eb9ef3bec9237b6
santosupadhyay <itsmr.santoshupadhyay18@gmail.com>
2024-04-21 14:58:34 +0545

    layout updated

--- practiceproject/src/Layout.jsx
+++ practiceproject/src/Layout.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import {Outlet} from 'react-router-dom'
+
 
 export default function Layout() {
   return (
     <div>
-      
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
   )
 }
