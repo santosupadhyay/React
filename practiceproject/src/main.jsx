2bf7041af0c1947819a8ae15c0415a26c7c0aa3b
santosupadhyay <itsmr.santoshupadhyay18@gmail.com>
2024-04-21 14:59:04 +0545

    main file updated

--- practiceproject/src/main.jsx
+++ practiceproject/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter, createRoutesFromChildren } from 'react-router-dom'
+import Layout from './Layout.jsx'
+
+
+const router =  createBrowserRouter(
+  createRoutesFromChildren(
+    <Route path= '/' element= {<Layout />} >
+    {/* <Route path='' element= {<Home/>} /> */}
+    {/* <Route path='about' element= {<About/>} />
+    <Route path='projects' element= {<Projects/>} />
+    <Route path='socialmedia' element= {<Socialmedia/>} />
+    <Route path='contact' element={<Contact/>} /> */}
+  </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode >
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
